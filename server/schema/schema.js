79f293b17d8672439edc98db4ae6ec3d2caff4da
nandor matyas <nandor.matyas@gmail.com>
2019-02-14 14:27:48 +0100

    feat(graphql): adding root queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,52 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { 
+  GraphQLObjectType,
+  GraphQLString, 
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type:GraphQLID } },
+      resolve(parent, args){
+        //code to get data from db
+        return 1
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type:GraphQLID } },
+      resolve(parent, args){
+        return 1
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
