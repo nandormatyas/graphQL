b5f50fc3038fb7989f86cfb9a0bdd39e0ad4fe53
nandor matyas <nandor.matyas@gmail.com>
2019-02-15 12:26:21 +0100

    feat(graphql): mongodb connected, added models, mutations

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb+srv://nandi:test123@cluster0-hekdv.mongodb.net/test?retryWrites=true');
+mongoose.connection.once('open', () => {
+  console.log('mongoo connected');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,6 @@
 const graphql = require('graphql');
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 const { 
   GraphQLObjectType,
@@ -72,6 +74,27 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt }
+      },
+      resolve(parent, args){
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        });
+        return author.save();
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation
 });
